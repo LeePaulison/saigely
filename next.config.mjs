90d22643aaa5cf705fcdd5dcd05d12da3144d3fe
LeePaulison <lee.paulison.jr@gmail.com>
2026-07-15 11:36:40 -0400

    Bundle GraphQL schemas in serverless deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {/* config options here */};
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/api/graphql": ["./graphql/schemas/**/*.graphql"],
+  },
+};
 
 export default nextConfig;
