5c086b2f1cef5450a315c0d61a494bb60271f751
LeePaulison <lee.paulison.jr@gmail.com>
2026-07-15 08:38:27 -0400

    cleanup

--- graphql/resolvers/aiAgents.js
+++ graphql/resolvers/aiAgents.js
@@ -1,7 +1,17 @@
-import { getAiAgents } from "@/repositories/aiAgentsRepository.js";
+import {
+  getAiAgentById,
+  getAiAgents,
+} from "@/repositories/aiAgentsRepository.js";
 
 export const aiAgentsResolvers = {
   Query: {
     aiAgents: (_) => getAiAgents(),
+    aiAgentConfiguration: (_, { agentId }, context) => {
+      if (!context.authenticated) {
+        throw new Error("Unauthorized");
+      }
+
+      return getAiAgentById(agentId);
+    },
   },
 };
